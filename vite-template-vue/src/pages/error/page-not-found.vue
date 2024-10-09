58d238482389e55c68732ed08ff6bc92f861900b
popup0221 <popup0221@naver.com>
2024-10-09 23:24:53 +0900

    feat: error 페이지

---
+++ vite-template-vue/src/pages/error/page-not-found.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-6xl font-bold text-red-600">404</h1>
+    <p class="mt-4 text-lg text-gray-700">이런! 찾고 있는 페이지가 없습니다.</p>
+    <router-link
+      to="/"
+      class="mt-6 text-blue-500 hover:underline">
+      집으로 돌아가기
+    </router-link>
+  </div>
+</template>
