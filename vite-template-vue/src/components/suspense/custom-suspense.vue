18fe6205073f2dd65c40f2c7cf147bfb6b992027
popup0221 <popup0221@naver.com>
2024-10-09 23:25:13 +0900

    chore: 파일명 컨벤션 변경

---
+++ vite-template-vue/src/components/suspense/custom-suspense.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Suspense } from 'vue'
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <slot name="default-slot" />
+    </template>
+
+    <template #fallback>
+      <slot name="fallback-slot">
+        <!-- receive skeleton comp -->
+      </slot>
+    </template>
+  </Suspense>
+</template>
---
+++ vite-template-vue/src/components/suspense/suspense-wrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface SuspenseWrapper {
+  asyncFn: (...params: unknown[]) => Promise<unknown> | Promise<void>
+}
+
+const props = defineProps<SuspenseWrapper>()
+
+await props.asyncFn()
+</script>
+
+<template>
+  <div>
+    <slot name="wrapper" />
+  </div>
+</template>
